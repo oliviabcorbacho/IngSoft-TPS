4b13ed5449d5a049653b1c0030504c67e01f1fe3
Olivia Browne Corbacho <ocorbacho@udesa.edu.ar>
2025-04-07 19:56:24 -0300

    Osea si pero no...

--- tp2/TP2-CorbachoSimian/src/anillo/Ring.java
+++ tp2/TP2-CorbachoSimian/src/anillo/Ring.java
@@ -2,65 +2,114 @@ package anillo;
 
 public class Ring {
 
-    private static class Node {
-        Object cargo;
-        Node next;
-
-        Node(Object cargo) {
-            this.cargo = cargo;
-            this.next = this;
-        }
+    private abstract static class Node {
+        abstract Node add(Object cargo);
+        abstract Node remove();
+        abstract Node next();
+        abstract Object current();
     }
 
-    private Node current;
-
-    private void emptyRingException() { //auxiliar
-        if (current == null) {
+    private static class EmptyNode extends Node { //NULL OBJECT PATTERN
+        Node add(Object cargo) {
+            return new SingleNode(cargo);
+        }
+        Node remove() {
+            throw new RuntimeException("Ring is empty");
+        }
+        Node next() {
+            throw new RuntimeException("Ring is empty");
+        }
+        Object current() {
             throw new RuntimeException("Ring is empty");
         }
     }
 
-    private void previous( Node desired){ //auxiliar
-        Node aux = current;
-        while (aux.next != current) {
-            aux = aux.next;
+    //JAJA EMILIO DIJO QUE ESTO NO ES VALIDO, tengo que hacer que singleNode y multiNode sean la misma clase
+    //Resuelvo estableciendo el elemento NEUTRO
+    private static class SingleNode extends Node {
+        private  Object cargo;
+
+        SingleNode(Object cargo) {
+            this.cargo = cargo;
         }
-        aux.next = desired;
-        current = desired;
+
+        Node add(Object cargo) {
+            MultiNode first = new MultiNode(cargo);
+            MultiNode second = new MultiNode(this.cargo);
+            first.next = second;
+            second.next = first;
+            return first;
+        }
+        Node remove() {
+            return new EmptyNode();
+        }
+        Node next() {
+            return this;
+        }
+        Object current() {
+            return cargo;
+        }
     }
 
+    private static class MultiNode extends Node {
+        private Object cargo;
+        private Node next;
+
+        MultiNode(Object cargo) {
+            this.cargo = cargo;
+            this.next = null;
+        }
+
+        Node add(Object newCargo) {
+            MultiNode newNode = new MultiNode(newCargo);
+            newNode.next = this;
+            Node prev = this;
+            while (((MultiNode) prev).next != this) {
+                prev = ((MultiNode) prev).next;
+            }
+            ((MultiNode) prev).next = newNode;
+            return newNode;
+        }
+
+        Node remove() {
+            Node prev = this;
+            while (((MultiNode) prev).next != this) {
+                prev = ((MultiNode) prev).next;
+            }
+            ((MultiNode) prev).next = next;
+
+            if (next == prev) {
+                return new SingleNode(((MultiNode) next).cargo);
+            } else {
+                return next;
+            }
+        }
+        Node next() {
+            return next;
+        }
+        Object current() {
+            return cargo;
+        }
+    }
+
+    private Node current = new EmptyNode();
+
     public Ring next() {
-        emptyRingException();
-        current = current.next;
+        current = current.next();
         return this;
     }
 
     public Object current() {
-        emptyRingException();
-        return current.cargo;
+        return current.current();
     }
 
     public Ring add(Object cargo) {
-        Node newNode = new Node(cargo);
-
-        if (current == null) {
-            current = newNode;
-        } else {
-            newNode.next = current;
-            previous(newNode);
-        }
+        current = current.add(cargo);
         return this;
     }
 
     public Ring remove() {
-        emptyRingException();
-
-        if (current.next == current) { // Only one element
-            current = null;
-        } else {
-            previous(current.next);
-        }
+        current = current.remove();
         return this;
     }
 }
-
